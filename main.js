0835606b877a57df150f38dcd682dd064caa83dc
edmundonajera4 <edmundonajera4@gmail.com>
2018-11-22 14:18:00 -0600

    initial comiit

---
+++ main.js
@@ -0,0 +1,15 @@
+// START
+
+
+function computerPlay(playerSelection, computerSelection) {
+    
+    console.log(playRound(playerSelection, computerSelection))
+}
+
+function play() {
+
+}
+
+game();
+
+// END
